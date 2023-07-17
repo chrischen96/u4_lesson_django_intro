832fa23b23e3c1593953b86b99336470d39ed9ee
chrischen <chrischen96@outlook.com>
2023-07-17 14:08:25 -0700

    XC

--- tunr_project_answer/settings.sql
+++ tunr_project_answer/settings.sql
@@ -1,4 +1,3 @@
--- settings.sql
 CREATE DATABASE tunr;
 CREATE USER tunruser WITH PASSWORD 'tunr';
 GRANT ALL PRIVILEGES ON DATABASE tunr TO tunruser;
--- tunr_project_answer/tunr_django/urls.py
+++ tunr_project_answer/tunr_django/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for tunr_django project.
+"""tunr_django URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('tunr.urls')),
+    path('api-auth', include('rest_framework.urls', namespace='rest_framework'))
 ]
